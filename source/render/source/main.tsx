7cca31f89cccecad3bb5300854c29f56aead8e00
yinxulai <yinxulai@hotmail.com>
2026-08-18 18:54:47 +0800

    feat: add a minimal gateway dashboard prototype page

--- source/render/source/main.tsx
+++ source/render/source/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { LinearPrototypePage } from './pages/linear-prototype/page'
 import './index.css'
+
+const isLinearPrototype = window.location.pathname === '/design/linear'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {isLinearPrototype ? <LinearPrototypePage /> : <App />}
   </React.StrictMode>,
 )
